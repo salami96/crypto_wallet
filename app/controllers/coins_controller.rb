de88f9d31cad62e1d84dca5612db8acecd7252e8
Gabriel Salami <gabriel.zanatto2@gmail.com>
2022-08-02 22:19:54 -0300

    feat(77): finish mining types

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: %i[ create new edit update ]
 
   # GET /coins or /coins.json
   def index
@@ -57,6 +58,9 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:acronym, :id)
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
@@ -64,6 +68,6 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
