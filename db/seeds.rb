ef00e6f4fe123bfa9596a4a037a604331bdefeaa
Gabriel Salami <gabriel.zanatto2@gmail.com>
2022-08-02 21:34:42 -0300

    feat(75): create minning types

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,33 @@ require 'tty-spinner'
 #   Character.create(name: 'Luke', movie: movies.first)
 
 [
+    { description: "Não se aplica", acronym: "N/a" },
+    { description: "Proof of Work", acronym: "PoW" },
+    { description: "Proof of Stake", acronym: "PoS" },
+    { description: "Proof of Capacity", acronym: "PoC" },
+].each_with_index { |mining_type, index| show_spinner("Cadastrando tipos de mineração ##{index}") { MiningType.find_or_create_by!(mining_type) } }
+
+[
     {
         description: "Bitcoin",
         acronym:     "BTC",
-        url_image:   "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=018"
+        url_image:   "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=018",
+        mining_type: MiningType.find_by_acronym("Pow")
     },
     {
         description: "Ethereum",
         acronym:     "ETH",
-        url_image:   "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=018"
+        url_image:   "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=018",
+        mining_type: MiningType.find_by_acronym("PoW")
     },
     {
         description: "USD Coin",
         acronym:     "USDC",
-        url_image:   "https://cryptologos.cc/logos/thumbs/usd-coin.png?v=022"
+        url_image:   "https://cryptologos.cc/logos/thumbs/usd-coin.png?v=022",
+        mining_type: MiningType.find_by_acronym("N/a")
+
     }
-].each { |coin| show_spinner("Cadastrando moedas") { Coin.find_or_create_by!(coin) } }
+].each_with_index { |coin, index| show_spinner("Cadastrando moedas ##{index}") { Coin.find_or_create_by!(coin) } }
 
 def show_spinner(init_msg)
   spinner = TTY::Spinner.new(":spinner rodando: #{init_msg} ...", format: :spin_3)
